01ded541fbed7b6aa2ada6544560a2e546e529c0
InWoo Lee <jopelee2@gmail.com>
2023-07-24 17:12:38 +0900

    controller, lombok test

---
+++ src/test/java/com/invu/web/HelloResponseDtoTest.java
@@ -0,0 +1,20 @@
+package com.invu.web;
+
+import com.invu.web.dto.HelloResponseDto;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombok_functioning_test() {
+        String name = "invu";
+        int amount = 30;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
